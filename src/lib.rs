8ba6a7639e444b25bf6dd0451b828799f9878142
Jose Ernesto Salazar <znx3p0@gmail.com>
2022-01-24 21:02:11 -0700

    use new version of canary and SRPC

--- src/lib.rs
+++ src/lib.rs
@@ -1,146 +1,125 @@
+
 use std::hash::Hash;
-use std::sync::Arc;
 
 use dashmap::DashMap;
+use canary::{prelude::*, routes::Route, Serialize};
 use serde::de::DeserializeOwned;
-use serde::Serialize;
-use canary::providers::ServiceAddr;
-use canary::routes::{Route, GLOBAL_ROUTE};
-use canary::{Addr, Channel, Result};
-use srpc::IntoClient;
+use srpc::prelude::*;
 
 const ID: &'static str = "sail";
 
 #[srpc::rpc]
-struct InnerSailDB<K: Hash + Eq, V> {
+pub struct SailDB<K, V> {
     map: DashMap<K, V>,
 }
 
-impl<K: Hash + Eq, V> InnerSailDB<K, V> {
-    pub fn new() -> Self {
-        InnerSailDB {
-            map: DashMap::new(),
-        }
+impl<K: Eq + Hash, V> Default for SailDB<K, V> {
+    fn default() -> Self {
+        Self { map: DashMap::new() }
     }
 }
 
 #[srpc::rpc(none)]
-impl<K: Hash + Eq, V> InnerSailDB<K, V> {
-    #[manual]
-    pub async fn get(&self, mut chan: Channel) -> Result<Channel> {
-        let key: K = chan.receive().await?;
-        let val = match self.map.get(&key) {
-            Some(val) => {
-                let val = val.value();
-                Some(val)
-            }
-            None => None,
-        };
-        chan.send(val).await?;
-        Ok(chan)
+impl<K: Hash + Eq, V> SailDB<K, V> {
+    #[server_manual]
+    async fn get(&self, mut c: Channel) -> Res<Channel> {
+        let key = c.receive().await?;
+        let val = self.map.get(&key)
+            .and_then(|s| Some(s.value()));
+        c.send(val).await?;
+        Ok(c)
     }
-    #[manual]
-    pub async fn insert(&self, mut chan: Channel) -> Result<Channel> {
-        let (key, value) = chan.receive().await?;
-        let value = self.map.insert(key, value);
-        chan.send(value).await?;
-        Ok(chan)
+    #[client_manual]
+    async fn get(c: &mut Channel, k: impl std::borrow::Borrow<K>) -> Res<Option<V>> {
+        c.send(k.borrow()).await?;
+        let res = c.receive().await?;
+        Ok(res)
     }
-    pub async fn insert_clean(&self, key: K, value: V) -> Result<Option<V>> {
-        let value = self.map.insert(key, value);
-        Ok(value)
+    #[server_manual]
+    async fn insert(&self, mut c: Channel) -> Res<Channel> {
+        let (key, val) = c.receive().await?;
+        let res = self.map.insert(key, val);
+        c.send(res).await?;
+        Ok(c)
     }
-    #[manual]
-    pub async fn remove(&self, mut chan: Channel) -> Result<Channel> {
-        let key: K = chan.receive().await?;
-        let value = self.map.remove(&key).and_then(|s| Some(s.1));
-        chan.send(value).await?;
-        Ok(chan)
+    #[client_manual]
+    async fn insert(c: &mut Channel, k: impl std::borrow::Borrow<K>, v: impl std::borrow::Borrow<V>) -> Res<Option<V>> {
+        c.send((k.borrow(), v.borrow())).await?;
+        let res = c.receive().await?;
+        Ok(res)
+    }
+
+    #[server_manual]
+    async fn remove(&self, mut c: Channel) -> Res<Channel> {
+        let key: K = c.receive().await?;
+        let res = self.map.remove(&key);
+        c.send(res).await?;
+        Ok(c)
+    }
+    #[client_manual]
+    async fn remove(c: &mut Channel, k: impl std::borrow::Borrow<K>) -> Res<Option<(K, V)>> {
+        c.send(k.borrow()).await?;
+        let res = c.receive().await?;
+        Ok(res)
     }
 }
 
-pub struct Sail<K: Hash + Eq, V> {
-    db: Option<InnerSailDBPeer<K, V>>,
-}
-
-impl<K, V> Sail<K, V>
+impl<K, V> SailDB<K, V>
 where
-    K: Hash + Eq + Serialize + DeserializeOwned + Sync + Send + 'static,
-    V: Serialize + DeserializeOwned + Sync + Send + 'static,
+    K: Hash + Eq + Serialize + DeserializeOwned,
+    V: Serialize + DeserializeOwned
 {
-    pub fn bind() -> Result<()>
+    pub fn bind() -> Res<()>
     where
         K: Send + Sync + 'static,
         V: Send + Sync + 'static,
     {
         Self::bind_at(ID)
     }
-    /// binds in global route
-    pub fn bind_at(at: &str) -> Result<()>
+    pub fn bind_at(at: &str) -> Res<()>
     where
         K: Send + Sync + 'static,
         V: Send + Sync + 'static,
     {
         Self::bind_in(&GLOBAL_ROUTE, at)
     }
-    pub fn bind_in(route: &Route, at: &str) -> Result<()>
+    pub fn bind_in(route: &Route, at: &str) -> Res<()>
     where
         K: Send + Sync + 'static,
         V: Send + Sync + 'static,
     {
-        route.add_service_at::<InnerSailDB<K, V>>(at, Arc::new(InnerSailDB::new()))
+        route.add_service_default_at::<Self>(at)
     }
-    pub async fn new(addr: Addr) -> Result<Self> {
+}
+
+pub struct Sail<K, V> {
+    db: SailDBPeer<K, V>,
+}
+
+impl<K: Hash + Eq, V> Sail<K, V> {
+    pub fn from_channel(chan: Channel) -> Self {
+        Sail { db: chan.client::<SailDB<K, V>>() }
+    }
+    pub async fn new_at(addr: &ServiceAddr) -> Res<Self> {
+        let chan = addr.connect().await?;
+        Ok(Self::from_channel(chan))
+    }
+    pub async fn new(addr: Addr) -> Res<Self> {
         let addr = addr.service(ID);
-        Self::new_at(addr).await
-    }
-    pub async fn new_at(addr: ServiceAddr) -> Result<Self> {
-        let db = addr.connect().await?.client::<InnerSailDB<K, V>>();
-        Ok(Sail { db: Some(db) })
-    }
-    pub async fn get(&mut self, k: &K) -> Result<Option<V>> {
-        let db = self.db.take().unwrap();
-        let mut chan: Channel = db.get().await?;
-        chan.send(k).await?;
-        let val: Option<V> = chan.receive().await?;
-        self.db = Some(chan.client::<InnerSailDB<K, V>>());
-        Ok(val)
-    }
-    pub async fn insert(&mut self, k: &K, v: &V) -> Result<Option<V>> {
-        let db = self.db.take().unwrap();
-        let mut chan = db.insert().await?;
-        chan.send((k, v)).await?;
-        let val = chan.receive().await?;
-        self.db = Some(chan.client::<InnerSailDB<K, V>>());
-        Ok(val)
-    }
-    pub async fn remove(&mut self, k: &K) -> Result<Option<V>> {
-        let db = self.db.take().unwrap();
-        let mut chan = db.remove().await?;
-        chan.send(k).await?;
-        let val = chan.receive().await?;
-        self.db = Some(chan.client::<InnerSailDB<K, V>>());
-        Ok(val)
+        Self::new_at(&addr).await
     }
 }
 
+impl <K, V> std::ops::Deref for Sail<K, V> {
+    type Target = SailDBPeer<K, V>;
 
-
-#[srpc::rpc] // other options include rpc(mutex), rpc(none)
-#[derive(Default)]
-pub struct DistributedList<T> {
-    list: Vec<T>
+    fn deref(&self) -> &Self::Target {
+        &self.db
+    }
 }
 
-#[srpc::rpc]
-impl<T: Clone> DistributedList<T> {
-    async fn push(&mut self, value: T) {
-        self.list.push(value);
-    }
-    async fn get(&self, index: usize) -> Option<T> {
-        self.list.get(index).and_then(|val| Some(val.clone()))
-    }
-    async fn remove(&mut self, index: usize) -> T {
-        self.list.remove(index)
+impl<K, V> std::ops::DerefMut for Sail<K, V> {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        &mut self.db
     }
 }
